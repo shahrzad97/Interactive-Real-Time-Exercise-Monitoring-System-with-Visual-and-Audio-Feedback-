1b8e83bdca0fe87c954a2c9f9a5f38c3b0341c70
Shahrzad Ali Khani Nejad <shahrzad.alikhaninejad@studenti.univr.it>
2025-09-20 21:52:14 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Interactive-Real-Time-Exercise-Monitoring-System-with-Visual-and-Audio-Feedback-/', 
 })
